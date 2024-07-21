b1f16187f1cff1cc640b60c9edc45249e48f14b2
angel <angeldrummer2000@gmail.com>
2024-07-21 09:30:05 -0700

    feat updated

--- index.js
+++ index.js
@@ -25,22 +25,53 @@ app.post('/api/users', function(req,res){
   res.json({ username: username, _id: _id })
 });
 
-app.get('api/users', function(req,res){
-  res.json(userDatabse);
+app.get('/api/users', function(req,res){
+  res.json(userDatabase);
 })
 
-app.post('api/users/:_id/exercises', function(req, res){
-  let id = req.body._id;
-  let description = req.body.description;
-  let duration = req.body.description;
-  let date = req.body.description;
+app.post('/api/users/:_id/exercises', function(req, res){
+  let userId = parseInt(req.params._id);
+  let user = userDatabase.find(user => user._id === userId);
 
-  if (id === userDatabase._id){
+  if (!user){
+    return res.json({ error: "User not found."})
+  }
+
+  let description = req.body.description;
+  let duration = parseInt(req.body.duration);
+  let date = req.body.date? new Date(req.body.date).toDateString(): new Date().toDateString();
+
+  if (!description || !duration){
+    return res.json({ error: "Description and duration are required."})
+  };
+
+
+  //wouldn't I have to use "req.body.description" to have the data that they provided on the sumbit form?
+  let exercise = {
+    description: description, 
+    duration: duration, 
+    date: date
+  };
+
+  let userLog = database.find(entry => entry._id === userId);
+  if (userLog){
+    userLog.log.push(exercise);
+    userLog.count++;
+  } else {
+    database.push({
+      username: user.username,
+      _id: userId,
+      count: 1,
+      log: [exercise]
+    })
+  }
+
+  if (userId === userDatabase._id){
     if (date){
       database.push({
       username: username,
       count: count,
-      _id: _id,
+      _id: userId,
       log: [{
         description: description,
         duration: duration,
@@ -51,7 +82,7 @@ app.post('api/users/:_id/exercises', function(req, res){
     res.json({
       username: username,
       count: count,
-      _id: _id,
+      _id: userId,
       log: [{
         description: description,
         duration: duration,
@@ -64,7 +95,7 @@ app.post('api/users/:_id/exercises', function(req, res){
       database.push({
         username: username,
         count: count,
-        _id: _id,
+        _id: userId,
         log: [{
           description: description,
           duration: duration,
@@ -75,7 +106,7 @@ app.post('api/users/:_id/exercises', function(req, res){
       res.json({
         username: username,
         count: count,
-        _id: _id,
+        _id: userId,
         log: [{
           description: description,
           duration: duration,
@@ -88,9 +119,20 @@ app.post('api/users/:_id/exercises', function(req, res){
   }
 })
 
-app.get('api/user/:_id/logs', function(req,res){
+app.get('/api/user/:_id/logs', function(req,res){
   let id = req.params._id
-  if (id === )
+  if (id === database._id){
+    res.json({
+      username: username,
+        count: count,
+        _id: _id,
+        log: [{
+          description: description,
+          duration: duration,
+          date: currentDate
+        }]
+    })
+  }
 })
 
 //exercise schema
